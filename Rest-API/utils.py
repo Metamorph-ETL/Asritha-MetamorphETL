21e711b8482f0a556d3207f02d4b31705b8e8b97
ashu-123-maker <asritha.vig2338@gmail.com>
2025-04-26 13:42:00 +0530

    Reading files from gcs buckets

--- Rest-API/utils.py
+++ Rest-API/utils.py
@@ -70,3 +70,6 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+    
