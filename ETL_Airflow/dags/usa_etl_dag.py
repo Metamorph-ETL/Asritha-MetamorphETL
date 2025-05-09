bd73f22af8bece8d28987430119c6e3b3aa17ded
ashu-123-maker <asritha.vig2338@gmail.com>
2025-05-09 21:35:43 +0530

    modified the ingestion tasks

--- ETL_Airflow/dags/usa_etl_dag.py
+++ ETL_Airflow/dags/usa_etl_dag.py
@@ -1,7 +1,7 @@
 from airflow.decorators import dag
 from datetime import datetime
 # Import the ETL task from the etl_tasks folder. Ensure your PYTHONPATH is set appropriately.
-from tasks.etl_task import m_load_usa
+from tasks.usa_population_ingestion import m_load_usa
 
 @dag(dag_id ="etl_taskflow",schedule_interval='@daily', start_date=datetime(2024, 1, 1), catchup=False, tags=['etl', 'spark'])
 def etl_task():
